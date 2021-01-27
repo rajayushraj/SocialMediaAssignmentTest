8e10921909e84388ea42fb1d1d08fb2fd020ded7
Ayush Raj <ayush1224raj@gmail.com>
2021-01-27 13:48:19 +0530

    Modified user model and friendship model and created associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :friend_sent , class_name: 'Friendship' ,foreign_key: 'sender_id',inverse_of: 'sender',dependent: :destroy
+  has_many :friends, -> {  Friendship.friends },through: :friend_sent,source: :receiver
+  has_many :friend_received ,class_name: 'Friendship',foreign_key: 'receiver_id',inverse_of: 'receiver',dependent: :destroy
+  has_many :friendsrec, -> { Friendship.friends },through: :friend_received,source: :sender
+  has_many :friend_request, -> { Friendship. not_friends },through: :friend_received,source: :sender
 end
